44c1479d173b2e1e64e12d83665a68a699ccf729
Saikat Mitra <80327464+saikatmitra1998@users.noreply.github.com>
2023-07-06 23:19:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -27,31 +27,31 @@ def dashboard():
     # Define your BigQuery query with the random task
     query = f"""
             SELECT
-  d.ClientID,
-  d.ClientName,
-  '{selected_task}' AS TaskType,
-  i.UniqueTeams,
-  p.predicted_label*{selected_quantity} AS TimeTakenInHours,
-  d.Distance__km_ AS DistanceInKms,
-  d.Time__hrs_ AS TimeTakenToReachInHours
-FROM (
-  SELECT DISTINCT UniqueTeams
-  FROM MwwMsSAPData.Inputdata_train2
-) AS i
-LEFT JOIN ML.PREDICT(MODEL `MwwMsSAPData.TimePredictionModelwithoutteamid`, (
-  SELECT
-    '{selected_task}' AS TaskType,
-    UniqueTeams
-  FROM (
-    SELECT DISTINCT UniqueTeams
-    FROM MwwMsSAPData.Inputdata_train2
-  )
-)) AS p
-ON i.UniqueTeams = p.UniqueTeams
-CROSS JOIN proj-iv-maintenance-service.SampleTestingDataset.DistancesAndTimeServicePointToClient AS d
-WHERE d.ClientID = '{selectedClient}'
-ORDER BY p.predicted_label ASC;
-"""
+              d.ClientID,
+              d.ClientName,
+              '{selected_task}' AS TaskType,
+              i.UniqueTeams,
+              p.predicted_label*{selected_quantity} AS TimeTakenInHours,
+              d.Distance__km_ AS DistanceInKms,
+              d.Time__hrs_ AS TimeTakenToReachInHours
+            FROM (
+                  SELECT DISTINCT UniqueTeams
+                  FROM MwwMsSAPData.Inputdata_train2
+                ) AS i
+            LEFT JOIN ML.PREDICT(MODEL `MwwMsSAPData.TimePredictionModelwithoutteamid`, (
+            SELECT
+                '{selected_task}' AS TaskType,
+                UniqueTeams
+                FROM (
+                    SELECT DISTINCT UniqueTeams
+                    FROM MwwMsSAPData.Inputdata_train2
+                      )
+                )) AS p
+                ON i.UniqueTeams = p.UniqueTeams
+            CROSS JOIN proj-iv-maintenance-service.SampleTestingDataset.DistancesAndTimeServicePointToClient AS d
+            WHERE d.ClientID = '{selectedClient}'
+            ORDER BY p.predicted_label ASC;
+            """
 
     # Execute the query and get the results as a Pandas DataFrame
     dataframe = client.query(query).to_dataframe()
